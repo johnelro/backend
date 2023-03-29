2625007f883f42a01f623b63b768a59a6a11ae91
ELRO -Admin <johnelrokarl@gmail.com>
2023-03-29 09:53:21 +0800

    Added Store functionality in CarouselItemsController

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,6 @@ Route::get('/carousel', [CarouselItemsController::class, 'index']);
 
 Route::get('/carousel/{id}', [CarouselItemsController::class, 'show']);
 
+Route::post('/carousel', [CarouselItemsController::class, 'store']);
+
 Route::delete('/carousel/{id}', [CarouselItemsController::class, 'destroy']);
