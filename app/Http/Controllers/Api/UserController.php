b38ff5cd0263b7a5bab1a9a03c4ee3039d761778
ELRO -Admin <johnelrokarl@gmail.com>
2023-03-30 10:04:37 +0800

    Added Index, Show, Store, Destroy functionality in UserController

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserRequest $request)
+    {
+        // Retrieve the validated input data...
+        $validated = $request->validated();
+
+        $validated['password'] = Hash::make($validated['password']);
+
+        $user = User::create($validated);
+
+        return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return User::findOrFail($id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        
+        $user = User::findOrFail($id);
+ 
+        $user->delete();
+
+        return $user;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\CarouselItemsController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+// Carousel Routes
 Route::get('/carousel', [CarouselItemsController::class, 'index']);
 
 Route::get('/carousel/{id}', [CarouselItemsController::class, 'show']);
@@ -28,3 +29,14 @@ Route::post('/carousel', [CarouselItemsController::class, 'store']);
 Route::put('/carousel/{id}', [CarouselItemsController::class, 'update']);
 
 Route::delete('/carousel/{id}', [CarouselItemsController::class, 'destroy']);
+
+//User Routes
+
+Route::get('/user', [UserController::class, 'index']);
+
+Route::post('/user', [UserController::class, 'store']);
+
+Route::get('/user/{id}', [UserController::class, 'show']);
+
+Route::delete('/user/{id}', [UserController::class, 'destroy']);
+
